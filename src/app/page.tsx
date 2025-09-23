e49925cb1c0dd9f5b4b68f9c8206c43fa5156e72
vincxxdev <vincxxdev@gmail.com>
2025-09-23 11:21:38 +0200

    Added hero and skills section into page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import Skills from "./components/Skills";
+import Hero from "./components/Hero";
+
 export default function Home() {
   return (
-    <div></div>
+    <main style={{ backgroundColor: 'var(--secondary-background)' }} className="min-h-screen">
+      <Hero />
+      <Skills />
+    </main>
   );
 }
